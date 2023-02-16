8bd83993e6c3720ddd2a93339d2c756519d5a834
Taylor Marvin <taylor@marvin.email>
2023-02-15 22:51:23 -0800

    feat: Update OpenTelemetry tracing to use ConfigureOpenTelemetryTracerProvider. Begin deprecation of AddLog4NetAndOpenTelemetry, break out into separate methods, Add AddOpenTelemetryMetrics functionality.

--- src/Logging/WerkrLoggingExtensions.cs
+++ src/Logging/WerkrLoggingExtensions.cs
@@ -6,49 +6,46 @@ using log4net.Config;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using OpenTelemetry;
+using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
 namespace Werkr.Common.Logging {
     public static class WerkrLoggingExtensions {
+
+        [Obsolete( "This method is obsolete - it has been replaced with separate AddLog4Net and AddOpenTelemetry methods." )]
         public static WebApplicationBuilder AddLog4NetAndOpenTelemetry(
+            this WebApplicationBuilder builder,
+            string serviceName,
+            string configPath = null
+        ) => builder
+                .AddOpenTelemetryTracing( serviceName )
+                .AddLog4Net( serviceName, configPath );
+
+        /// <summary>
+        /// Reads the ${AppContext.BaseDirectory}/log4net.config file and configures a Log4NetProvider.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="serviceName"></param>
+        /// <param name="configPath"></param>
+        /// <returns></returns>
+        public static WebApplicationBuilder AddLog4Net(
             this WebApplicationBuilder builder,
             string serviceName,
             string configPath = null
         ) {
             configPath ??= Path.Combine( AppContext.BaseDirectory, "log4net.config" );
             ConfigureFromLog4NetXmlConfigFile( new FileInfo( configPath ) );
-
-            IConfigurationSection logSection = builder.Configuration.GetSection( "Logging" );
-            // Add Open Telemetry Tracing
-            if (logSection["OTLP:EnableTelemetry"]?.ToLower( ) == "true") {
-                _ = builder
-                    .Services
-                    .AddOpenTelemetry( )
-                    .WithTracing(
-                        builder => builder
-                                    .AddHttpClientInstrumentation( )
-                                    .AddAspNetCoreInstrumentation( )
-                                    .AddGrpcClientInstrumentation( )
-                                    .AddSource( serviceName )
-                                    .SetResourceBuilder( ResourceBuilder.CreateDefault( ).AddService( serviceName ) )
-                                    .AddOtlpExporter( a =>
-                                        a.Endpoint = new Uri( logSection["OTLP:CollectorAddress"] )
-                                    )
-                    ).StartWithHost( );
-            }
-
             _ = builder.Logging.AddProvider( new Log4NetProvider( new WerkrILogger( serviceName ) ) );
-            _ = builder.Logging.AddOpenTelemetry( );
 
             return builder;
         }
 
+
         /// <summary>
         /// Ensures the <paramref name="configFile"/> exists then uses
         /// <seealso cref="XmlConfigurator.Configure(ILoggerRepository,FileInfo)"/> to configure the
-        /// <see cref="LoggerRepository"/>.
+        /// <see cref="log4net.Repository.ILoggerRepository"/>.
         /// </summary>
         /// <param name="configFile"></param>
         private static void ConfigureFromLog4NetXmlConfigFile( FileInfo configFile ) {
@@ -62,6 +59,69 @@ namespace Werkr.Common.Logging {
                 LogManager.GetRepository( Assembly.GetEntryAssembly( ) ),
                 configFile
             );
+        }
+
+        /// <summary>
+        /// Enables OpenTelemetry tracing if the "OTLP:EnableTelemetry" logsection is true.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        public static WebApplicationBuilder AddOpenTelemetryTracing(
+            this WebApplicationBuilder builder,
+            string serviceName
+        ) {
+            IConfigurationSection logSection = builder.Configuration.GetSection( "Logging" );
+            // Add Open Telemetry Tracing
+            if (logSection["OTLP:EnableTelemetry"]?.ToLower( ) == "true") {
+                // Configure OpenTelemetry tracing.
+                _ = builder.Services.ConfigureOpenTelemetryTracerProvider(
+                    ( isp, tpb ) => {
+                        _ = tpb
+                            .AddSource( serviceName )
+                            .AddHttpClientInstrumentation( )
+                            .AddAspNetCoreInstrumentation( )
+                            .AddGrpcClientInstrumentation( )
+                            .SetResourceBuilder( ResourceBuilder.CreateDefault( ).AddService( serviceName, null, GetAssemblyVersion( ) ) )
+                            .AddOtlpExporter( a => a.Endpoint = new Uri( logSection["OTLP:CollectorAddress"] ) );
+                    }
+                );
+                _ = builder.Logging.AddOpenTelemetry( );
+            }
+            return builder;
+        }
+
+        /// <summary>
+        /// Retrieves the entry assembly file version if its available.
+        /// </summary>
+        private static string GetAssemblyVersion( ) => (null == Assembly.GetEntryAssembly( ))
+            ? null
+            : System.Diagnostics.FileVersionInfo.GetVersionInfo( Assembly.GetEntryAssembly( )!.Location ).FileVersion;
+
+        /// <summary>
+        /// Enables OpenTelemtry metrics if the "OTLP:EnableTelemetry" logsection is true.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        public static WebApplicationBuilder AddOpenTelemetryMetrics(
+            this WebApplicationBuilder builder,
+            string serviceName
+        ) {
+            IConfigurationSection logSection = builder.Configuration.GetSection( "Logging" );
+            // Add Open Telemetry Tracing
+            if (logSection["OTLP:EnableTelemetry"]?.ToLower( ) == "true") {
+                // Configure OpenTelemetry metrics.
+                _ = builder.Services.ConfigureOpenTelemetryMeterProvider(
+                    ( isp, tpb ) => {
+                        _ = tpb
+                            .AddMeter( serviceName )
+                            .AddOtlpExporter( a => a.Endpoint = new Uri( logSection["OTLP:CollectorAddress"] ) );
+                    }
+                );
+                _ = builder.Logging.AddOpenTelemetry( );
+            }
+            return builder;
         }
     }
 }
